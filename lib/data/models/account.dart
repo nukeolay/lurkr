023b3b5b205a5b733237eb7587380994f091ced6
nukeolay <66204571+nukeolay@users.noreply.github.com>
2021-04-06 22:15:45 +0300

    creating

--- lib/data/models/account.dart
+++ lib/data/models/account.dart
@@ -1,18 +1,14 @@
-import 'dart:convert';
-import 'dart:html';
-import 'package:http/http.dart' as http;
-
-class AccountList {
-  List<Account> accounts;
-
-  AccountList({required this.accounts});
-
-  factory AccountList.fromJson(Map<String, dynamic> json) {
-    List accountJson = json['users'] as List;
-    List<Account> accountList = accountJson.map((i) => Account.fromJson(i['user'])).toList();
-    return AccountList(accounts: accountList);
-  }
-}
+// class AccountList {
+//   List<Account> accounts;
+//
+//   AccountList({required this.accounts});
+//
+//   factory AccountList.fromJson(Map<String, dynamic> json) {
+//     List accountJson = json['users'] as List;
+//     List<Account> accountList = accountJson.map((i) => Account.fromJson(i['user'])).toList();
+//     return AccountList(accounts: accountList);
+//   }
+// }
 
 class Account {
   final String username;
@@ -22,6 +18,8 @@ class Account {
   final String fullName;
   final bool isVerified;
   final bool hasAnonymousProfilePicture;
+  final bool isNew;
+  final DateTime lastTimeUpdated;
 
   Account(
       {required this.username,
@@ -30,21 +28,25 @@ class Account {
       required this.pk,
       required this.fullName,
       required this.isVerified,
-      required this.hasAnonymousProfilePicture});
+      required this.hasAnonymousProfilePicture,
+      required this.isNew,
+      required this.lastTimeUpdated});
 
-  factory Account.fromJson(Map<String, dynamic> inputJson) {
+  factory Account.fromApi(Map<String, dynamic> inputJson) {
     return Account(
         username: inputJson['username'] as String,
-        profilePicUrl: inputJson['profile_pic_url'] as Uri,
+        profilePicUrl: Uri.parse(inputJson['profile_pic_url']),
         isPrivate: inputJson['is_private'] as bool,
         pk: inputJson['pk'] as String,
         fullName: inputJson['full_name'] as String,
         isVerified: inputJson['is_verified'] as bool,
-        hasAnonymousProfilePicture: inputJson['has_anonymous_profile_picture'] as bool);
+        hasAnonymousProfilePicture: inputJson['has_anonymous_profile_picture'] as bool,
+        isNew: false,
+        lastTimeUpdated: DateTime.now());
   }
 
   @override
   String toString() {
-    return 'name: $username, private: $isPrivate';
+    return 'name: $username, private: $isPrivate, isNew: $isNew';
   }
 }
